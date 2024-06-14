0994fabe289160f91bce89147ae11c3c96e1b6ec
AlejandroWaiz <alejosaurio1@gmail.com>
2024-06-14 00:35:22 -0400

    Adding database connection

---
+++ database/createDatabase.go
@@ -0,0 +1,47 @@
+package database
+
+import (
+	"context"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/mtslzr/pokeapi-go"
+	"github.com/mtslzr/pokeapi-go/structs"
+)
+
+func (d *Database) createDatabase() []error {
+
+	var errors []error
+
+	resources, err := pokeapi.Resource("pokemon", 0, 1300)
+
+	if err != nil {
+		log.Printf("[Database] Err getting pokeapi resources: %v", err)
+		errors = append(errors, err)
+		return errors
+	}
+
+	var pokemon structs.Pokemon
+
+	for i, r := range resources.Results {
+
+		pokemon, err = pokeapi.Pokemon(r.Name)
+
+		if err != nil {
+			log.Printf("Err getting %v data: %v", r.Name, err)
+			errors = append(errors, err)
+		}
+
+		_, err := d.dbClient.Collection(os.Getenv("pokeapi_pokemons_collection")).Doc(strconv.Itoa(i)).Set(context.Background(), pokemon)
+
+		if err != nil {
+			log.Printf("Err saving %v data: %v", r.Name, err)
+			errors = append(errors, err)
+		}
+
+	}
+
+	return errors
+
+}
--- database/database.go
+++ database/database.go
@@ -3,12 +3,16 @@ package database
 import (
 	"context"
 	"fmt"
+	"os"
 
+	"cloud.google.com/go/firestore"
 	firebase "firebase.google.com/go/v4"
+	"google.golang.org/api/option"
 )
 
 type Database struct {
-	db *firebase.App
+	app      *firebase.App
+	dbClient *firestore.Client
 }
 
 type DatabaseImplementation interface {
@@ -16,13 +20,20 @@ type DatabaseImplementation interface {
 
 func New() (DatabaseImplementation, error) {
 
-	db, err := firebase.NewApp(context.Background(), nil)
+	firestoreApp, err := firebase.NewApp(context.Background(), nil, option.WithCredentialsFile(os.Getenv("service_account_path")))
 
 	if err != nil {
-		err = fmt.Errorf("[Database] Error initializing: %v\n", err)
+		err = fmt.Errorf("[Database] Error initializing firebase: %v\n", err)
 		return nil, err
 	}
 
-	return &Database{db: db}, nil
+	firestoreClient, err := firestoreApp.Firestore(context.Background())
+
+	if err != nil {
+		err = fmt.Errorf("[Database] Error initializing firestore: %v\n", err)
+		return nil, err
+	}
+
+	return &Database{app: firestoreApp, dbClient: firestoreClient}, nil
 
 }
